855c430d9cabe9069ad7336737506f00e3da9b8e
Joseph Lanham <jslanham2020@gmail.com>
2024-01-16 17:14:32 -0500

    dom

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const h1 = document.createElement("h1");
+h1.id = 'victory';
+h1.textContent = 'is the champion';
+const newHeader = h1
+document.body.append(newHeader);
